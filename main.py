8752d73a6d212508ead742a7d962d7138fbd09c6
Yohann Pirbay <pirbay.yohann@gmail.com>
2024-08-04 23:06:19 +0100

    add Deck class

--- main.py
+++ main.py
@@ -1,37 +1,41 @@
 import random
 
-cards = []
-suits = ["spades", "clubs", "hearts", "diamonds"]
-ranks = [
-    {"rank": "A", "value": 11},
-    {"rank": "2", "value": 2},
-    {"rank": "3", "value": 3},
-    {"rank": "4", "value": 4},
-    {"rank": "5", "value": 5},
-    {"rank": "6", "value": 6},
-    {"rank": "7", "value": 7},
-    {"rank": "8", "value": 8},
-    {"rank": "9", "value": 9},
-    {"rank": "10", "value": 10},
-    {"rank": "J", "value": 10},
-    {"rank": "Q", "value": 10},
-    {"rank": "K", "value": 10},
-]
+class Deck():
 
-for suit in suits:
-    for rank in ranks:
-        cards.append([suit, rank])
+    def __init__(self):
+        self.cards = []
+        suits = ["spades", "clubs", "hearts", "diamonds"]
+        ranks = [
+            {"rank": "A", "value": 11},
+            {"rank": "2", "value": 2},
+            {"rank": "3", "value": 3},
+            {"rank": "4", "value": 4},
+            {"rank": "5", "value": 5},  
+            {"rank": "6", "value": 6},
+            {"rank": "7", "value": 7},
+            {"rank": "8", "value": 8},
+            {"rank": "9", "value": 9},
+            {"rank": "10", "value": 10},
+            {"rank": "J", "value": 10},
+            {"rank": "Q", "value": 10},
+            {"rank": "K", "value": 10},
+        ]
+    
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append([suit, rank])
 
-def shuffle():
-    random.shuffle(cards)
+    def shuffle(self):
+        random.shuffle(self.cards)
 
-def deal(number):
-    dealt_cards = []
-    for i in range(number):
-        dealt_cards.append(cards.pop())
-    return dealt_cards
-
-shuffle()
-card = deal(1)[0]
-
-print(card)
+    def deal(self, number):
+        dealt_cards = []
+        for i in range(number):
+            dealt_cards.append(self.cards.pop())
+        return dealt_cards
+    
+deck_one = Deck()
+deck_two = Deck()
+deck_one.shuffle()
+print(deck_one.deal(1))
+print(deck_two.deal(1))
