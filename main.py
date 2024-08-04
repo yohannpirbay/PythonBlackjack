bd7bd651ef1ee03132e588d15f3bd25cdd8c8054
Yohann Pirbay <pirbay.yohann@gmail.com>
2024-08-04 22:07:58 +0100

    implement dictionary approach for ranks

--- main.py
+++ main.py
@@ -2,7 +2,21 @@ import random
 
 cards = []
 suits = ["spades", "clubs", "hearts", "diamonds"]
-ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+ranks = [
+    {"rank": "A", "value": 11},
+    {"rank": "2", "value": 2},
+    {"rank": "3", "value": 3},
+    {"rank": "4", "value": 4},
+    {"rank": "5", "value": 5},
+    {"rank": "6", "value": 6},
+    {"rank": "7", "value": 7},
+    {"rank": "8", "value": 8},
+    {"rank": "9", "value": 9},
+    {"rank": "10", "value": 10},
+    {"rank": "J", "value": 10},
+    {"rank": "Q", "value": 10},
+    {"rank": "K", "value": 10},
+]
 
 for suit in suits:
     for rank in ranks:
@@ -18,17 +32,6 @@ def deal(number):
     return dealt_cards
 
 shuffle()
-cards_dealt = deal(2)
-card = cards_dealt[0]
-rank = card[1]
+card = deal(1)[0]
 
-if rank == "A":
-    value = 11
-elif rank == "J" or rank == "K" or rank == "Q":
-    value = 10
-else: 
-    value = int(rank)
-
-rank_dict = {"rank": rank, "value": value}
-
-print(rank_dict["rank"], rank_dict["value"])
+print(card)
